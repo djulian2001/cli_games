c46572c6d63ffe065f7fc9768dbd54f8675b333e
vagrant <vagrant@localhost.localdomain>
2017-10-20 17:09:01 +0000

    extraction of the game class tests

--- tests/test_game.py
+++ tests/test_game.py
@@ -2,8 +2,12 @@ import unittest
 import copy
 from random import shuffle
 
+from app.game import Game
+from app.game import Player
 
-class Test_Unittest(unittest.TestCase):
+
+class Test_Game(unittest.TestCase):
+# class Test_Unittest(unittest.TestCase):
   
   # Unittest general 
   def setUp(self):
@@ -12,11 +16,49 @@ class Test_Unittest(unittest.TestCase):
   def tearDown(self):
     """If there was a required clean up."""
     pass
-  def test_assert(self):
-    assert True
-  def test_assertFalse(self):
-    self.assertFalse(False)
+  
+  def test_game_interface(self):
+    g = Game(
+      name='my_game',
+      description="my cool game",
+      players=['Tim','Pup'],
+      total_players=2,
+      max_team_size=1 )
+    self.assertIsInstance( g, Game )
+    self.assertIs( type( g.name ), str )
+    # description
+    self.assertIs( type( g.description ), str )
+    # number of players
+    # number of teams
+    self.assertIs( type( g.total_players), int )
+    self.assertIs( type( g.max_team_size ), int )
+    self.assertIs( type( g.total_teams), int )
+    self.assertEqual( g.total_teams, 2)
+    self.assertEqual( len(g.players_out),0 )
 
+  def test_game_method_player_is_out(self):
+    g = Game(
+      name='adding',
+      description='add players',
+      players=['Teddy','Ruxin'] )
+
+    player = g.players[0]
+    g.player_is_out( player )
+    self.assertEqual( len( g.players ), 1 )
+    self.assertEqual( len( g.players_out ), 1 )
+
+  def test_game_method_set_players(self):
+    g = Game(
+      name='adding',
+      description='add players',
+      players=['Teddy','Ruxin'] )
+
+    self.assertIsInstance( g.players[0], Player )
+    self.assertEqual( len(g.players), 2 )
+
+  def test_game_method_exit_game(self):
+    g = Game('help','help',['bob'])
+    self.assertTrue( hasattr( g, "exit_game" ) )
 
 
 if __name__=="__main__":
--- tests_app.py
+++ tests_app.py
@@ -1,1 +1,2 @@
 from tests.test_game import *
+from tests.test_card_game import *
